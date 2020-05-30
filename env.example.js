b5c5259a15db62366364b72c65e5be88356dc7e2
root <root@root.com>
2020-05-30 14:44:00 +0800

    update

---
+++ env.example.js
@@ -0,0 +1,14 @@
+window.settings = {
+  // 站点标题
+  title: 'V2Board',
+  // API
+  host: '',
+  // 主题
+  theme: {
+    sidebar: 'light',
+    header: 'dark',
+    color: 'default'
+  },
+  // 背景
+  background_url: ''
+}
